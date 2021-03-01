dd2ed8073820fa9268a4f86e21d6629ca3910a7b
KaptainWutax <kaptainwutax@gmail.com>
2021-03-01 18:17:33 -0500

    nether ruined portal

--- src/main/java/kaptainwutax/featureutils/structure/RuinedPortal.java
+++ src/main/java/kaptainwutax/featureutils/structure/RuinedPortal.java
@@ -7,15 +7,27 @@ import kaptainwutax.seedutils.mc.VersionMap;
 
 public class RuinedPortal extends UniformStructure<RuinedPortal> {
 
-	public static final VersionMap<RegionStructure.Config> CONFIGS = new VersionMap<RegionStructure.Config>()
+	public static final VersionMap<RegionStructure.Config> OVERWORLD_CONFIGS = new VersionMap<RegionStructure.Config>()
 			.add(MCVersion.v1_16, new RegionStructure.Config(40, 15, 34222645));
 
-	public RuinedPortal(MCVersion version) {
-		this(CONFIGS.getAsOf(version), version);
+	public static final VersionMap<RegionStructure.Config> NETHER_CONFIGS = new VersionMap<RegionStructure.Config>()
+			.add(MCVersion.v1_16, new RegionStructure.Config(25, 10, 34222645));
+
+	public RuinedPortal(Dimension dimension, MCVersion version) {
+		this(getConfigs(dimension).getAsOf(version), version);
 	}
 
 	public RuinedPortal(RegionStructure.Config config, MCVersion version) {
 		super(config, version);
+	}
+
+	public static VersionMap<RegionStructure.Config> getConfigs(Dimension dimension) {
+		switch(dimension) {
+			case OVERWORLD: return OVERWORLD_CONFIGS;
+			case NETHER: return NETHER_CONFIGS;
+		}
+
+		return new VersionMap<>();
 	}
 
 	@Override
--- src/main/java/kaptainwutax/featureutils/structure/generator/StrongholdGenerator.java
+++ src/main/java/kaptainwutax/featureutils/structure/generator/StrongholdGenerator.java
@@ -13,7 +13,6 @@ import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
 public class StrongholdGenerator {
 
