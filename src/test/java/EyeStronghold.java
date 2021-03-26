23148a3826cb9daed0474a89d41677d74e0f50e4
Neil <h66r10@gmail.com>
2021-03-26 03:32:06 +0100

    stronghold eyes

---
+++ src/test/java/EyeStronghold.java
@@ -0,0 +1,15 @@
+import kaptainwutax.featureutils.structure.generator.StrongholdGenerator;
+import kaptainwutax.seedutils.mc.ChunkRand;
+import kaptainwutax.seedutils.mc.MCVersion;
+
+import java.util.Arrays;
+
+public class EyeStronghold {
+    public static void main(String[] args) {
+        StrongholdGenerator g=new StrongholdGenerator(MCVersion.v1_16);
+
+        ChunkRand chunkRand=new ChunkRand().asChunkRandDebugger();
+        g.populateStructure(42,-75,61,chunkRand);
+        System.out.println(Arrays.toString(g.getEyes()));
+    }
+}
